44d47d51c86ce4bc1a7ac6567c26c0e93690981f
VitharanageVihanga <vihangasathsara214@gmail.com>
2026-02-05 02:02:08 +0000

    Delete fyp-project directory

--- fyp-project/scripts/achives/data_preparation/check_merge.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-df = pd.read_csv("/Users/vihangasathsara/Desktop/fyp-project/data/output/merged_hmrc_ons_totals.csv")
-print("Missing ONS values:", df['import_value_million_gbp'].isna().sum())
-
